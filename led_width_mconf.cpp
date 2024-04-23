014c66d8b07f399d7815f504f9aeb4aa83fcfed0
WiktorK02 <wiktor.kidon@hotmail.com>
2024-04-23 15:26:45 +0200

    adjustment for ESP32

--- led_width_mconf.cpp
+++ led_width_mconf.cpp
@@ -1,41 +1,63 @@
+
 #include <FastLED.h>
 
-#define LED_PIN                 2
+#define LED_PIN                 1
 #define NUM_LEDS                51
 #define BRIGHTNESS              255
-#define ANALOG_PIN              A0
-#define ANALOG_RANGE_PIN        A1
-#define ANALOG_BRIGHTNESS_PIN   A2
-#define ANALOG_COLOR_PIN        A3  
 
 CRGB leds[NUM_LEDS];
 int led_range = 0;
+const int analogInPin1 = 9;
+const int analogInPin2 = 8;
+const int analogInPin3 = 7;
+const int analogInPin4 = 2;
 
+const int numReadings = 30;  // Number of readings to average
+int sensorReadings1[numReadings];
+int sensorReadings2[numReadings];
+int sensorReadings3[numReadings];
+int sensorReadings4[numReadings];
+int currentIndex = 0;  // Renamed from 'index'
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS);
-  pinMode(ANALOG_PIN, INPUT);
-  pinMode(ANALOG_RANGE_PIN, INPUT);
-  pinMode(ANALOG_BRIGHTNESS_PIN, INPUT);
-  pinMode(ANALOG_COLOR_PIN, INPUT);  
+  analogReadResolution(8);
+
+  pinMode(analogInPin1, INPUT_PULLUP);
+  pinMode(analogInPin2, INPUT_PULLUP);
+  pinMode(analogInPin3, INPUT_PULLUP);
+  pinMode(analogInPin4, INPUT_PULLUP);
 }
 
 void loop() {
-  int analogValue = analogRead(ANALOG_PIN);
-  int mappedValue = map(analogValue, 532, 666, 0, NUM_LEDS - 1);
+  int sensorValue1 = 255 - analogRead(analogInPin1); //color
+  int sensorValue2 = 252 - analogRead(analogInPin2); //brightness
+  int sensorValue3 = 112 - analogRead(analogInPin3); //range
+  int sensorValue4 = 79 - analogRead(analogInPin4); //position
 
-  int rangeValue = analogRead(ANALOG_RANGE_PIN);
-  led_range = map(rangeValue, 640, 666, 0, 10);
-  mappedValue = constrain(mappedValue, led_range, NUM_LEDS - 1 - led_range);
+  // Add the new readings to the array
+  sensorReadings1[currentIndex] = sensorValue1;
+  sensorReadings2[currentIndex] = sensorValue2;
+  sensorReadings3[currentIndex] = sensorValue3;
+  sensorReadings4[currentIndex] = sensorValue4;
 
-  int analog_brightness = analogRead(ANALOG_BRIGHTNESS_PIN);
-  int mapped_analog_brightness = map(analog_brightness, 0, 666, 255, 0);
+  // Increment the currentIndex or wrap around
+  currentIndex = (currentIndex + 1) % numReadings;
+
+  // Calculate the average
+  int total1 = 0, total2 = 0, total3 = 0, total4 = 0;
+  for (int i = 0; i < numReadings; i++) {
+    total1 += sensorReadings1[i];
+    total2 += sensorReadings2[i];
+    total3 += sensorReadings3[i];
+    total4 += sensorReadings4[i];
+  }
+  int hueValue = total1 / numReadings;
+  int mapped_analog_brightness = total2 / numReadings;
+  int led_range = total3 / numReadings;
+  int mappedValue = total4 / numReadings;
   if (mapped_analog_brightness < 0) mapped_analog_brightness = 0;
   FastLED.setBrightness(mapped_analog_brightness);
-
-  int hueValue = analogRead(ANALOG_COLOR_PIN);  
-  hueValue = map(hueValue, 0, 666, 0, 255);
-
   int red = 0;
   int green = 0;
   int blue = 0;
@@ -68,6 +90,7 @@ void loop() {
     leds[ledIndex].green = min(leds[ledIndex].green + brightnessIncrement, green);
     leds[ledIndex].blue = min(leds[ledIndex].blue + brightnessIncrement, blue);
   }
-
+  Serial.println(sensorValue1);
+  FastLED.delay(1);
   FastLED.show();
 }
